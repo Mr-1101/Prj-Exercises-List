50643e5bd800fed4734346d030e8fbe513cb2dad
Mr-1101 <marashh69@gmail.com>
2025-11-07 05:51:59 +0330

    calculate is finish first part

--- prj-01/01.py
+++ prj-01/01.py
@@ -1,7 +1,28 @@
-#------------ variables------------
+# variables------------
+
 fNumber = 0
 sNumber = 0
 result = 0
 action = ""
 
+# input------------------
 
+fNumber = int(input("plz enter the first number ---> "))
+action = input("plz tell what you want to do - + * / ---> ")
+sNumber = int(input("plz enter the second number ---> "))
+
+#core-------------------
+if action == '+':
+    result = fNumber + sNumber 
+    print (f"your  result is :{result}")
+elif action == '-':
+    result = fnumber - sNumber
+    print (f"your  result is :{result}")
+elif action == '*':
+    result = fNumber * sNumber
+    print (f"your  result is :{result}")
+elif action == '/':
+    result = fNumber / sNumber
+    print (f"your  result is :{result}")
+else:
+    print('write the corect value !!! ')
