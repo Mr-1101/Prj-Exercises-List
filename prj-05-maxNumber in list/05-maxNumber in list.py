4bf8fcc4a3d9a19e8844ef1d59c084efea4e661e
Mr-1101 <marashh69@gmail.com>
2025-11-08 23:24:49 +0330

    end the prj05

--- prj-05-maxNumber in list/05-maxNumber in list.py
+++ prj-05-maxNumber in list/05-maxNumber in list.py
@@ -1,19 +1,36 @@
 # variablr ------------------------------
-maxNumber = 0
 getNumbers = ""
-counter = 0
+counter = -1
+indexCounter = 0
+
 # main ---------------------------------
+
+
 sNumbers = input("plz enter your numbers like 1,2,3,4,..... ---> ")
 getNumbers = sNumbers.split(",")
 numbers = [int(item) for item in getNumbers]
 
-while numbers[counter] <= len(numbers):
-    print( numbers[counter], len(numbers))
+
+
+'''
+maxNumber = numbers[0]
+
+while indexCounter <= len(numbers):
     for i in numbers:
-        if numbers[counter] > maxNumber:
+        if indexCounter <= len(numbers):
+            i > maxNumber
             maxNumber = i
-            counter += 1
+            counter = indexCounter
+        indexCounter += 1
 
     print(f"your max number in the list is {maxNumber} it has in this {counter} place of array")
+'''
 
+# other way -------------------------------------
 
+maxNum = max(numbers)
+
+for i in numbers:
+    if i != 100:
+        counter += 1
+print(f"your max number is {maxNum} ,and this number.s index {counter}")
