c1d23ed22da1df2a1582db3e2e883bfbe5172135
Mr-1101 <marashh69@gmail.com>
2025-11-17 23:47:04 +0330

    check password is make do the generate pass

---
+++ prj-08-password algoritm/08-password-algoritem.py
@@ -0,0 +1,43 @@
+# variable -----------------------------------------
+yourPassword = ""
+# main---------------------------------------------
+
+def checkPassword():
+    
+    passUpper = 0
+    passLower = 0
+    passNumbers = 0
+    passSymbol = 0
+    symbol = ["@","!","#","$","%","&"]
+
+    yourPassword = input("enter your password to check level ---> ")
+    if len(yourPassword) == 12:
+        for a in yourPassword :
+            if a.isupper():
+                passUpper = 1
+            elif a.islower():
+                passLower = 1
+            elif a.isdigit():
+                passNumbers = 1
+            elif a in symbol:
+                passSymbol = 1
+        
+        result = passUpper + passLower + passNumbers + passSymbol
+        if result == 1 :
+            print("your password is  so weak")
+        elif result == 2 :
+            print("your password is weak")
+        elif result == 3:
+            print("your password is good")
+        else:
+            print("your password is strong")
+
+        print(f"your password level is {result}")
+
+
+    else:
+        print("your password less or more than 12 characters")
+
+
+
+checkPassword()
